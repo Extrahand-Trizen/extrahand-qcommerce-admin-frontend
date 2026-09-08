552282c4751bfe8b963c8b4c822623709b4a3d91
Asish <asishvenkat.a2004@gmail.com>
2026-09-08 14:38:46 +0530

    updates

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -37,7 +37,6 @@ const catalogueNav: NavItem[] = [
     icon: Package,
     children: [
       { name: 'Products', href: '/products' },
-      { name: 'Product Submissions', href: '/product-submissions' },
     ],
   },
 ];
